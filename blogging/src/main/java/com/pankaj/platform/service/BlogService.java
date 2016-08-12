c3bd21a49cc60ac77bc44c5f9363b2f5c4fdcfed
Pankaj Pardasani <Pankaj.Pardasani@barclays.com>
2016-08-12 15:38:00 +0100

    initial commit

---
+++ blogging/src/main/java/com/pankaj/platform/service/BlogService.java
@@ -0,0 +1,11 @@
+package com.pankaj.platform.service;
+
+
+import com.pankaj.platform.dto.APIOptions;
+
+/**
+ * Created by pankajpardasani on 17/07/2016.
+ */
+public interface BlogService {
+    void addNewBlogEntry(APIOptions options);
+}
---
+++ blogging/src/main/java/com/pankaj/platform/transformer/InternalTransformer.java
@@ -0,0 +1,16 @@
+package com.pankaj.platform.transformer;
+
+
+import com.pankaj.platform.domain.Blog;
+import com.pankaj.platform.entity.BlogEntity;
+
+/**
+ * Created by pankajpardasani on 17/07/2016.
+ */
+public class InternalTransformer {
+
+    public static BlogEntity transformBlog(Blog blog) {
+        return null;
+
+    }
+}
