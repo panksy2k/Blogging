0ba8b096d69466ffb6ed33c6f3d6e6b2f167753c
Pankaj Pardasani <pankaj.d.p@me.com>
2016-08-14 14:46:33 +0100

    Added login page and its controller

---
+++ src/main/java/com/pankaj/platform/domain/AdminUser.java
@@ -0,0 +1,63 @@
+package com.pankaj.platform.domain;
+
+/**
+ * Created by pankajpardasani on 14/08/2016.
+ */
+public class AdminUser {
+
+
+    private String email;
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private boolean adminUser;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isAdminUser() {
+        return adminUser;
+    }
+
+    public void setAdminUser(boolean adminUser) {
+        this.adminUser = adminUser;
+    }
+}
