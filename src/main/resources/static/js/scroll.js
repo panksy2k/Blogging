2eea655519e5e25a850df3c7fab0259814e46387
Pankaj Pardasani <pankaj.d.p@me.com>
2016-09-01 18:00:32 +0100

    changed js function to fix scrolling

--- src/main/resources/static/js/scroll.js
+++ src/main/resources/static/js/scroll.js
@@ -1,13 +1,22 @@
 /**
  * Created by pankajpardasani on 01/09/2016.
  */
-$('body').scrollspy({ target: '.navbar',
-    offset: 160 });
 
-//smooth scrolling
-$('nav a').bind('click', function () {
-    $('html, body').stop().animate({
-        scrollTop: $($(this).attr('href')).offset().top - 100
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
+$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    $('body').scrollspy({ target: '#bs-example-navbar-collapse-1',
+        offset: 160 });
+
+    //smooth scrolling
+    $('nav a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
 });
+
+
